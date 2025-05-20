d9952d1c20480d0deb98038c829c5757485fd1f2
devyani-keche <devyanik2006@gmail.com>
2025-05-21 00:50:24 +0530

    modified:   app/index.jsx 	renamed:    assets/images/react-logo.png -> assets/images/logo.png

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,13 @@
-import { ScrollView, Text, Touchable, TouchableOpacity, View } from "react-native";
+import { ScrollView, StatusBar, Text, Touchable, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {Image} from "react-native";
-import logo from "../assets/images/react-logo.png";
+import logo from "../assets/images/logo.png";
 export default function Index() {
   const router = useRouter();
   return (
     <SafeAreaView className={`bg-[#f3d3d9]`}>
+      <StatusBar barStyle={"light-content"} backgroundColor={"#f3d3d9"} />
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View>
           <Image
